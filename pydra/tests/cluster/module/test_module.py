523731b9e6ae9e065714b506d6bdd84edec3e175
Peter Krenesky <peter@osuosl.org>
2010-09-05 20:50:08 -0700

    Tests - tests for Module

---
+++ pydra/tests/cluster/module/test_module.py
@@ -0,0 +1,75 @@
+"""
+    Copyright 2009 Oregon State University
+
+    This file is part of Pydra.
+
+    Pydra is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Pydra is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Pydra.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import unittest
+
+from pydra.cluster.module import Module
+
+
+class TestModule(module):
+    shared = ['foo']
+    
+class TestModule2(module):
+    shared = ['foo']
+
+class ModuleTestCase(unittest.TestCase):
+    
+    def test_shared_attributes(self):
+        """
+        tests shared attributes:
+        
+        Verifies:
+            * setting and getting attribute from different modules
+        """
+        manager = ModuleManager()
+        module0 = TestModule()
+        module1 = TestModule2()
+        
+        manager.register(module0)
+        manager.register(module1)
+        
+        module0.foo = 123
+        self.assertEqual(module0.foo, 123)
+        self.assertEqual(module1.foo, 123)
+        
+        module1.foo = 456
+        self.assertEqual(module0.foo, 456)
+        self.assertEqual(module1.foo, 456)
+    
+    def test_register(self):
+        """
+        Tests register callback
+        
+        Verifies:
+            * manager is set and unset
+        """
+        manager = ModuleManager()
+        module = TestModule()
+        
+        module._register(manager)
+        self.assertEqual(module.manager, manager)
+        
+        module.deregister()
+        self.assertEqual(module.manager, None)
+    
+    def test_emit(self):
+        """
+        Tests emitting a signal
+        """
+        
