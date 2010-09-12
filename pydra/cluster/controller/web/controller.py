4511bb9f0b3954de3693c6f678855860682cc662
Peter Krenesky <peter@osuosl.org>
2010-09-12 16:48:47 -0700

    Controller API - various fixes to WebController     * Controller now caches wrapped functions for faster reuse     * Controller now maps wrapped functions in a better way so that the class can be extended more easily     * WebControllerFunction.make_key() now uses self.key removing the need to pass the value in     * WebControllerFunction._authenticate now calls _authenticate and challenge_response like other functions, removing the need to create a new instance of WebControllerFunction every time it is called     * comment formatting

--- pydra/cluster/controller/web/controller.py
+++ pydra/cluster/controller/web/controller.py
@@ -28,6 +28,7 @@ from pydra.cluster.controller import ControllerException, \
     CONTROLLER_ERROR_DISCONNECTED, CONTROLLER_ERROR_NO_RSA_KEY, \
     CONTROLLER_ERROR_AUTH_FAIL
 
+
 class WebControllerFunction():
     """
     Proxy to a remote function exposed to the interface.  Receiving this
@@ -38,6 +39,11 @@ class WebControllerFunction():
     additional requests to authenticate with the master if needed.
     """
     def __init__(self, controller, key):
+        """
+        :parameters:
+            controller: controller this function is associated with
+            key: function name this object wraps
+        """
         self.controller = controller
         self.key = key
     
@@ -50,50 +56,48 @@ class WebControllerFunction():
             values = {'args':simplejson.dumps(args),
                       'kwargs':simplejson.dumps(kwargs)}
             data = urllib.urlencode(values)
-            url = self.controller.opener.open(self.make_url(self.key), data)
+            url = self.controller.opener.open(self.make_url(), data)
             return simplejson.loads(url.read())
         except urllib2.HTTPError, e:
             if e.code == 404:
                 raise ControllerException(CONTROLLER_ERROR_FUNCTION_NOT_FOUND)
             elif e.code == 401:
-                return self.__authenticate(*args, **kwargs)
+                return self._authenticate(*args, **kwargs)
             elif e.code == 500:
-                errors = simplejson.loads(e.read())
-                print errors['exception']
+                errors = simplejson.loads(e.msg)
                 raise ControllerRemoteException(errors)
             else:
                 raise ControllerException()
         except urllib2.URLError, e:
             raise ControllerException(CONTROLLER_ERROR_DISCONNECTED)
 
-    def make_url(self, key):
+    def make_url(self):
         return 'https://%s:%s/%s' % (self.controller.host,
                                     self.controller.port,
-                                    key)
+                                    self.key)
 
-    def __authenticate(self, *args, **kwargs):
+    def _authenticate(self, *args, **kwargs):
         """
         Authenticate with server and then recall this WebControllerFunction
         if successful
         """
-
+        
         # If priv_key is None, then master.key didn't get loaded correctly.
         if self.controller.priv_key is None:
             raise ControllerException(CONTROLLER_ERROR_NO_RSA_KEY)
-
-        challenge_func = WebControllerFunction(self.controller, 'authenticate')
-        challenge = challenge_func().__str__()
-
+        
+        # get challenge string from server
+        challenge = str(self.controller.authenticate())
+        
         # re-encrypt using servers key and then sha hash it before sending it
         # back
         response_encode = self.controller.priv_key.encrypt(challenge, None)
         response_hash = hashlib.sha512(response_encode[0]).hexdigest()
         
-        challenge_func = WebControllerFunction(self.controller, \
-                                               'challenge_response')
-        if challenge_func(response_hash):
+        # if challenge is successful, call this function again
+        if self.controller.challenge_response(response=response_hash):
             return self(*args, **kwargs)
-
+        
         raise ControllerException(CONTROLLER_ERROR_AUTH_FAIL)
 
 
@@ -102,27 +106,33 @@ class WebController(object):
     Controller for interacting with pydra web interface
     """
     
-    def __init__(self, host, port=18801, key='./master.key'):
+    def __init__(self, host='localhost', port=18801, key='./master.key'):
         """
         Initialize WebController
         
-        @host - host of interface
-        @port - port of interface
-        @key - path to key file for authentication [./master.key]
+        :parameters:
+            host:  host of interface
+            port:  port of interface
+            key:  path to key file for authentication [./master.key]
         """
         self.host = host
         self.port = port
-
+        
         # load rsa crypto
         self.pub_key, self.priv_key = load_crypto(key, False)
-        
         cj = cookielib.CookieJar()
         self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
 
     def __getattribute__(self, key):
         """
-        Overloaded to return methods as WebControllerFunctions
+        Overridden to return remote (unknown) methods as WebControllerFunctions.
+        The functions are cached on the controller so that repeat calls will
+        use the existing WebControllerFunction instance.
         """
-        if key in ('host', 'port', 'pub_key', 'priv_key', 'opener'):
+        if key == '__dict__' or key in self.__dict__.keys():
             return object.__getattribute__(self, key)
-        return WebControllerFunction(self, key)
+        
+        # wrap and cache remote function
+        func =  WebControllerFunction(self, key)
+        self.__dict__[key] = func
+        return func
