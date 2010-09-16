2298029943e151d91ea4d0e2a79ace8dc9514f45
Corbin Simpson <simpsoco@osuosl.org>
2010-09-16 15:46:53 -0700

    D'oh! Undo commit of my local Pydra settings.
    ( >&)

--- config/pydra_settings.py
+++ config/pydra_settings.py
@@ -6,10 +6,10 @@ TEMPLATE_DEBUG = DEBUG
 
 # Base directory for storing all files created at runtime
 # this includes encryption keys, logs, tasks, etc
-RUNTIME_FILES_DIR = '/home/simpson/pydra/lib'
+RUNTIME_FILES_DIR = '/var/lib/pydra'
 
 # Directory where process ids are stored.
-RUNTIME = '/home/simpson/pydra/run'
+RUNTIME = '/var/run/pydra'
 
 
 
@@ -23,8 +23,6 @@ DATABASE_HOST = ''                                  # Set to empty string for lo
 DATABASE_PORT = ''                                  # Set to empty string for default.
 
 
-# absolute path to the docroot of this site
-DOC_ROOT = '/home/simpson/pydra/pydra_site'
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -59,7 +57,7 @@ INSTALLED_APPS = (
 #Logging
 import logging
 LOG_LEVEL = logging.DEBUG
-LOG_DIR = '/home/simpson/pydra/log'
+LOG_DIR = '/var/log/pydra'
 LOG_FILENAME_MASTER = '%s/master.log' % LOG_DIR
 LOG_FILENAME_NODE   = '%s/node.log' % LOG_DIR
 LOG_SIZE = 10000000
